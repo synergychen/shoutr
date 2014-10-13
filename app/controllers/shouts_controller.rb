846f4469f4753880131d901ad5f54f70cbcaaf07
Chen Huang <alan.tolearn@gmail.com>
2014-10-13 11:27:23 -0400

    Add strong params and associates User and Shouts
    * user has many shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -7,7 +7,13 @@ class ShoutsController < ApplicationController
   end
 
   def create
-    Shout.create(user_id: current_user.id, body: params[:shout][:body])
+    current_user.shouts.create(shout_params)
     redirect_to shouts_path
+  end
+
+  private
+
+  def shout_params
+    params.require(:shout).permit(:body)
   end
 end
