8860d4e107a86e480cf43186c2ef7578d965afa2
Chen Huang <alan.tolearn@gmail.com>
2014-10-13 11:03:51 -0400

    Users can create shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,5 +1,13 @@
 class ShoutsController < ApplicationController
+  before_action :require_login
+
   def index
+    @shout = Shout.new
     @shouts = Shout.all
+  end
+
+  def create
+    Shout.create(user_id: current_user.id, body: params[:shout][:body])
+    redirect_to shouts_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :shouts, only: [:create, :index]
 
   root "shouts#index"
 end
