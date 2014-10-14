fcc3e294996e4e2bb3f415bc8a4be09619c50aa7
Chen Huang <alan.tolearn@gmail.com>
2014-10-14 14:33:04 -0400

    Validate users to not follow themselves
    * add custom validate user_is_not_follow_themself

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,3 +1,11 @@
 class FollowingRelationship < ActiveRecord::Base
   belongs_to :followed_user, class_name: "User"
+
+  validate :user_is_not_follow_themself
+
+  def user_is_not_follow_themself
+    if follower_id == followed_user_id
+      errors.add(:base, "User can't follow themselves")
+    end
+  end
 end
