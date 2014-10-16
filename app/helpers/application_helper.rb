f1a010eda1af82b8e044f4bbd7a7fb515f956b01
Chen Huang <alan.tolearn@gmail.com>
2014-10-16 17:04:25 -0400

    Add link and bold username if exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
   def highlight(text)
-    text.gsub(/@\w+/) { |result| "<strong>#{result}<\/strong>"}
+    text.gsub(/@\w+/) { |result| link_if_exist(result) }
+  end
+
+  private
+
+  def link_if_exist(result)
+    user = User.find_by(username: result[1..-1])
+    if user
+      "<strong>#{ link_to result, user_path(user) }<\/strong>"
+    else
+      result
+    end
   end
 end
--- app/models/text_subject.rb
+++ app/models/text_subject.rb
@@ -2,8 +2,4 @@ class TextSubject < ActiveRecord::Base
   has_one :shout, as: :subject
 
   validates :body, presence: true
-
-  def body=(new_body)
-    self[:body] = new_body.upcase
-  end
 end
